274b0e761aa72247d3528273db59589c6a7e9af0
gavin-rose <gavin.rose@debugr.net>
2020-03-24 00:19:37 -0500

    auth0

--- lib/deckcom_web/controllers/auth_controller.ex
+++ lib/deckcom_web/controllers/auth_controller.ex
@@ -9,28 +9,36 @@ defmodule DeckcomWeb.AuthController do
   
     def logout(conn, _params) do
       conn
+      #|> put_flash(:info, "You have been logged out!")
       |> configure_session(drop: true)
       |> redirect(external: "https://debugr.auth0.com/v2/logout?returnTo=https%3A%2F%2Fblooming-retreat-92794.herokuapp.com%2F&client_id=z0lsC3TDK6yW8GRxWzeqiafH69Ly0XoC")
     end
   
     def callback(%{assigns: %{ueberauth_failure: _fails}} = conn, _params) do
       conn
+      #|> put_flash(:error, "Failed to authenticate.")
       |> redirect(to: "/")
     end
   
     def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
       case UserFromAuth.find_or_create(auth) do
         {:ok, user} ->
+          n = Enum.random(1111111..9999999)
           if Users.get_user_by_email(email: user.email) === nil do
             Users.create_user(%{full_name: user.name, email: user.email})
           end
+          sidUser = Users.get_user_by_email(email: user.email)
+          sid = sidUser.id
+          IO.puts n 
           link = "/"
           conn
+          #|> put_flash(:info, "Successfully authenticated as " <> user.email <> ".")
           |> put_session(:current_user, user)
           |> redirect(to: link)
           
         {:error, reason} ->
           conn
+          #|> put_flash(:error, reason)
           |> redirect(to: "/")
       end
     end
