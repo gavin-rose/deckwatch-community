27408b02991fc70312895a4c8b9af11687ddcc65
gavin-rose <gavin.rose@debugr.net>
2020-03-27 13:24:07 -0500

    asdf

--- lib/deckcom_web/controllers/deck_controller.ex
+++ lib/deckcom_web/controllers/deck_controller.ex
@@ -4,6 +4,21 @@ defmodule DeckcomWeb.DeckController do
   alias Deckcom.Decks
   alias Deckcom.Decks.Deck
 
+  plug :secure
+  defp secure(conn, _params) do
+    user = get_session(conn, :current_user)
+    case user do
+    #can people get in without logging in? ask team
+     nil ->
+         conn 
+         #|> assign(:current_user, nil) #
+         |> redirect(to: "/auth/auth0") |> halt
+     _ ->
+       conn
+       |> assign(:current_user, user)
+    end
+  end
+
   def index(conn, _params) do
     decks = Decks.list_decks()
     render(conn, "index.html", decks: decks)
