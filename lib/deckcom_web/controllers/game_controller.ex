43fec8a33ec46ec58c1ef2cdc896e09c937b8457
gavin-rose <gavin.rose@debugr.net>
2020-03-29 15:02:36 -0500

    asdf

--- lib/deckcom_web/controllers/game_controller.ex
+++ lib/deckcom_web/controllers/game_controller.ex
@@ -4,6 +4,21 @@ defmodule DeckcomWeb.GameController do
   alias Deckcom.Games
   alias Deckcom.Games.Game
 
+  plug :secure
+  defp secure(conn, _params) do
+    user = get_session(conn, :current_user)
+    case user do
+    #can people get in without logging in? ask team
+     nil ->
+         conn 
+         #|> assign(:current_user, nil) #
+         |> redirect(to: "/auth/auth0") |> halt
+     _ ->
+       conn
+       |> assign(:current_user, user)
+    end
+  end
+
   def index(conn, _params) do
     games = Games.list_games()
     render(conn, "index.html", games: games)
