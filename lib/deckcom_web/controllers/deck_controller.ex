88f71247a736e75407357f37253646237f4ad9d7
gavin-rose <gavin.rose@debugr.net>
2020-04-10 23:00:22 -0500

    deck add parsing

--- lib/deckcom_web/controllers/deck_controller.ex
+++ lib/deckcom_web/controllers/deck_controller.ex
@@ -32,24 +32,7 @@ defmodule DeckcomWeb.DeckController do
   def create(conn, %{"deck" => deck_params}) do
     case Decks.create_deck(deck_params) do
       {:ok, deck} ->
-        cards = String.split(elem(Map.fetch(deck_params, "in_play_text"), 1), "\n", parts: :infinity)
-        cardsList = []
-        for card <- cards do
-          dets = String.split(card, " ", parts: 2)
-          name = Enum.at(dets, 1)
-          IO.puts name
-          IO.puts inspect cid = Enum.at(Deckcom.Cards.get_cards(name: name), 0)
-          IO.puts inspect elem(Map.fetch(cid, :id), 1)
-          quantity = String.replace(Enum.at(dets, 0), "x", "") |> String.to_integer
-          IO.puts inspect "QUANTITY: #{quantity}"
-          if cid != nil do
-            if deck.play === nil do
-              Decks.update_deck(deck, %{play: [elem(Map.fetch(cid, :id), 1)]})
-            else
-              Decks.update_deck(deck, %{play: deck.play ++ [elem(Map.fetch(cid, :id), 1)]})
-            end
-          end
-        end
+        String.split(elem(Map.fetch(deck_params, "in_play_text"), 1), "\r", parts: :infinity) |> run_play(0, deck.id)
         conn
         |> put_flash(:info, "Deck created successfully.")
         |> redirect(to: Routes.deck_path(conn, :show, deck))
@@ -91,5 +74,27 @@ defmodule DeckcomWeb.DeckController do
     conn
     |> put_flash(:info, "Deck deleted successfully.")
     |> redirect(to: Routes.deck_path(conn, :index))
+  end
+
+  def run_play(list, number, did) do
+    deck = Decks.get_deck!(did)
+    card = Enum.at(list, number)
+    if card != nil do 
+      dets = String.split(card, " ", parts: 2)
+      name = Enum.at(dets, 1)
+      cid = Enum.at(Deckcom.Cards.get_cards(name: name), 0)
+      quantity = String.replace(String.replace(Enum.at(dets, 0), "\n", ""), "x", "") |> String.to_integer
+      IO.puts inspect "QUANTITY: #{quantity}"
+      if cid != nil do
+        lis = deck.play
+        if lis === nil do
+          lis = []
+        end
+        Decks.update_deck(deck, %{play: lis ++ [elem(Map.fetch(cid, :id), 1)]})
+      else
+        {:ok}
+      end
+      run_play(list, number + 1, did)
+    end
   end
 end
